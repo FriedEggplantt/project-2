90c6665896154da158b108939b9528c997ca6083
moa <lagneskog.moa@elev.ga.ntig.se>
2022-03-11 14:57:42 +0100

    coin början

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const character = document.getElementById("character");
 const pen = document.getElementById("pen");
+const coin = document.getElementById("coin");
+const coin = 0;
+const coindisplay = document.querySelector('coin');
 
 function jump() {
   if (character.classList != "jump") {
@@ -20,7 +23,11 @@ let Alive = setInterval(function () {
   console.log("characterTop: " + characterTop);
   if (penLeft < 100 && penLeft > 0 && characterTop >= 200) {
     alert("YOU GOT PEN PIERCED!");
-  }
+}
+if(coinLeft < 100 && coinLeft > 0 && characterTop >= 200){
+coin++;
+coindisplay.innerHTML = coin;
+};
 }, 10);
 
 document.addEventListener("keydown", function (event) {
