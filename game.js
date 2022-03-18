f8e0cf6c29a362cad6dc0091ce6c67757af41619
moa <lagneskog.moa@elev.ga.ntig.se>
2022-03-18 14:51:50 +0100

    DONE!

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
 const character = document.getElementById("character");
 const pen = document.getElementById("pen");
-const coin = document.getElementById("coin");
-const coin = 0;
-const coindisplay = document.querySelector('coin');
+let hops = 0;
+const hopscount = document.getElementById("hops");
 
 function jump() {
   if (character.classList != "jump") {
+    hops++;
+    hopscount.innerHTML = hops;
     character.classList.add("jump");
     setTimeout(function () {
       character.classList.remove("jump");
@@ -22,12 +23,10 @@ let Alive = setInterval(function () {
   console.log("penLeft: " + penLeft);
   console.log("characterTop: " + characterTop);
   if (penLeft < 100 && penLeft > 0 && characterTop >= 200) {
-    alert("YOU GOT PEN PIERCED!");
-}
-if(coinLeft < 100 && coinLeft > 0 && characterTop >= 200){
-coin++;
-coindisplay.innerHTML = coin;
-};
+    alert("YOU GOT PEN PIERCED AFTER " + hops + " JUMPS");
+    hops = 0;  
+    hopscount.innerHTML = hops;
+  }
 }, 10);
 
 document.addEventListener("keydown", function (event) {
